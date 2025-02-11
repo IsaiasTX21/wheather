e06f685ca20fc381971404e4c3af6c0535bcfa25
IsaiasTX21 <isaiasnovaera@gmail.com>
2025-02-11 18:58:55 -0300

    Update weather.jsx

--- src/weather.jsx
+++ src/weather.jsx
@@ -15,7 +15,7 @@ navigate(`/city/${search}`)
             <div  style={{ height:"100vh", backgroundRepeat:"no-repeat", backgroundSize:"cover",backgroundImage: `url(https://images.unsplash.com/photo-1583978618388-c9726a050ac1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3MDUzNzV8MHwxfHNlYXJjaHw0fHxyaW8lMjBkZSUyMGphbmVpcm9zfGVufDB8fHx8MTczODcxOTM2Nnww&ixlib=rb-4.0.3&q=80&w=1080)` }} >
             <div  style={{maxWidth:"400px"}}  className=' painel row  d-flex container  m-auto'>
                 <div className='position-relative text-center'>
-                    <input onChange={(e)=> setseach(e.target.value) } style={{ position:"relative" ,height: "37px",  width: "200px", display: "inline" }} className='form-control m-auto'></input>
+                    <input placeholder="New York, Rio de Janeiro , etc..." onChange={(e)=> setseach(e.target.value) } style={{ position:"relative" ,height: "37px",  width: "200px", display: "inline" }} className='form-control m-auto'></input>
                     <button onClick={city} style={{  Width: "300px", position:"relative", bottom:"2px"}} className='btn btn-primary'>search</button>
                         <br/>
                     </div>
